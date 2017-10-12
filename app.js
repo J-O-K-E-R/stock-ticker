845178a806cdced870b63440cf6ed32be5ea1de5
Thomas Wyatt <tomfwyatt@gmail.com>
2017-10-11 19:08:46 -0700

    seed trends collection

--- app.js
+++ app.js
@@ -26,6 +26,24 @@ let rollInterval = 3000;
 //     const rollInterval = 600000;
 // }
 
+// UNCOMMENT THIS AND RUN SERVER ONCE TO SEED THE DATABASE WITH THE TRENDS MODEL
+//
+// const Trends = require("./models/trend.js");
+// const arr = [
+//     {"index":0,"name":"grain", "history":[]},
+//     {"index":1,"name":"industrial", "history":[]},
+//     {"index":2,"name":"bonds", "history":[]},
+//     {"index":3,"name":"oil", "history":[]},
+//     {"index":4,"name":"silver", "history":[]},
+//     {"index":5,"name":"gold", "history":[]}
+// ]
+// Trends.create(arr, function(err, docs) {
+//     if (err) {
+//         console.log("shit's fucked yo");
+//     }
+// });
+// 
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("port", (process.env.PORT || 5000));
 app.use("/static", express.static(__dirname + "/static"));
